035c8b5dcc0f0d54a3c0a72b0a79a80b01a74819
Lena <tortik@mail.ru>
2023-03-03 11:53:42 +0300

    ..

--- lab03/Program.cs
+++ lab03/Program.cs
@@ -12,6 +12,15 @@ a = Convert.ToDouble(Console.ReadLine());
 b = Convert.ToDouble(Console.ReadLine());
 c = Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine($" ввeдите x 1 x2 ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+for (; x1 < x2; x1++) 
+{
+    Console.WriteLine($" x = {x1}, y= {a*Math.Pow(x1,2 )+ b*x1 + c}");
+}
+
 double x0 = -b / (2 * a);
 Console.WriteLine($"x0 = {x0}");
 double y0 = a * Math.Pow(x0, 2) + b * x0 + c;
@@ -48,7 +57,7 @@ if (a > 0)
 
 if (n <= 0)
 {
-    Console.WriteLine($"{a > 0} функц всегда + ");
+    Console.WriteLine($"{a > 0} функц всегда + "); 
 }
 
 else if (n <= 0)
@@ -56,11 +65,12 @@ else if (n <= 0)
     Console.WriteLine($"{a < 0} функц всегда - ");
 }
 
-double x1 = -b + Math.Sqrt(n) / 2 * a;
-double x2 = b - Math.Sqrt(n) / 2 * a;
+double x3 = -b + Math.Sqrt(n) / 2 * a;
+double x4 = b - Math.Sqrt(n) / 2 * a;
 
 if (n >= 0)
 {
-    Console.WriteLine(x1);
-    Console.WriteLine(x2);
+    Console.WriteLine($" вычисление вершин функц ");
+    Console.WriteLine(x3); 
+    Console.WriteLine(x4);
 }
